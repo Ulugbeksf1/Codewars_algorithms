bfa08257050dc9a6a6d9ce5cba84c0418aeba160
Ulugbek Mirmukhamedov <90541949+Ulugbeksf1@users.noreply.github.com>
2021-09-12 00:32:33 -0700

    Update kata7.js

--- kata7.js
+++ kata7.js
@@ -1,1 +1,15 @@
 
+//Count the number of divisors of a non-negative integer n.
+
+//Random tests go up to n = 500000.
+
+function getDivisorsCnt(n){
+    // todo
+ let res =0;
+  for(let i=0; i<=n; i++){
+    if(n%i==0){
+      res++;
+    }
+  }
+  return res
+}
