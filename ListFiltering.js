69a7fd8591274e50d03643c039a595e5a63a00f4
Ulugbek Mirmukhamedov <90541949+Ulugbeksf1@users.noreply.github.com>
2021-09-15 11:01:17 -0700

    Update ListFiltering.js

--- ListFiltering.js
+++ ListFiltering.js
@@ -1,1 +1,5 @@
-
+//In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with t
+function filter_list(l) {
+  // Return a new array with the strings filtered out
+return  l.filter(e => typeof e ==="number")
+}
