7356d15b0ddaaaceb02362dbc2152ca96dc2307a
Ulugbek Mirmukhamedov <90541949+Ulugbeksf1@users.noreply.github.com>
2021-10-10 13:42:14 -0700

    Create keypadHorrow.js

---
+++ keypadHorrow.js
@@ -0,0 +1,15 @@
+/*Having two standards for a keypad layout is inconvenient!
+Computer keypad's layout:
+7 8 9  \n
+4 5 6  \n
+1 2 3  \n
+  0 \n
+
+Cell phone keypad's layout:
+1 2 3\n 
+4 5 6\n  
+7 8 9\n  
+  0\n*/
+function computerToPhone(numbers){
+ return numbers.replace(/[0-9]/g,v=>v==0?0:v==7?1:v==8?2:v==9?3:v==1?7:v==2?8:v==3?9:v)
+}
